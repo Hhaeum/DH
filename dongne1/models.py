94b573205a61b7fce7eca138ccc47297dc79fa82
Hhaeum <yellowgreeting@gmail.com>
2019-07-30 14:27:12 +0900

    3

--- dongne1/models.py
+++ dongne1/models.py
@@ -21,3 +21,4 @@ class Comment(models.Model):
     content=models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add= True)
     updated_at = models.DateTimeField(auto_now = True)
+
--- dongne1/views.py
+++ dongne1/views.py
@@ -6,4 +6,14 @@ def index(request):
 
 def daegu(request):
     return render(request,'daegu.html')
+
+def CService(request): 
+    posts = Blog.objects.all()
+    paginator = Paginator(posts,1) 
+    now_page = request.GET.get('page')
+    posts = paginator.get_page(now_page) 
+    context={ 
+        "posts":posts,  
+        }
+    return render(request,"CService.html",context)
 # Create your views here.
