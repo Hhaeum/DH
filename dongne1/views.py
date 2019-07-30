e0f0f7624bc5554a24af6ef23db30e97d061db09
Hhaeum <yellowgreeting@gmail.com>
2019-07-30 14:41:26 +0900

    4

--- dongne1/views.py
+++ dongne1/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
+from django.core.paginator import Paginator
 
 def index(request):
 
@@ -8,10 +10,10 @@ def daegu(request):
     return render(request,'daegu.html')
 
 def CService(request): 
-    posts = DH.objects.all()
+    posts = Blog.objects.all()
     paginator = Paginator(posts,1) 
     now_page = request.GET.get('page')
-    posts = paginator.get_page(now_page) 
+    posts = paginator.get_page(now_page)
     context={ 
         "posts":posts,  
         }
