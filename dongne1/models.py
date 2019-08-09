8fc93a08ee36ca9c5efdf7a8541d007ae7870cc5
Hhaeum <yellowgreeting@gmail.com>
2019-08-09 21:21:19 +0900

    123

--- dongne1/models.py
+++ dongne1/models.py
@@ -14,6 +14,17 @@ class Dongne1(models.Model):
     lat = models.FloatField(blank=True)
     lng = models.FloatField(blank=True)
 
+class Dongne2(models.Model):
+    user2 = models.ForeignKey(User,on_delete=models.CASCADE)
+    title2 = models.CharField(max_length = 200)
+    content2 = models.TextField(blank = True)
+    created_at2 = models.DateField(auto_now_add= True)
+    updated_at2 = models.DateField(auto_now = True)
+    category2 = models.CharField(max_length=50,blank=True)
+    anonymous2 = models.BooleanField(default=False)
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
+    pic2=models.ImageField(upload_to="image/",null=True)
+    
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     nickname=models.CharField(max_length=50,blank=True)
--- dongne1/views.py
+++ dongne1/views.py
@@ -41,7 +41,7 @@ def read(request,post_id):
     return render(request,'read.html',context)
 
 def CService(request): 
-    posts1 = Dongne1.objects.all()
+    posts1 = Dongne2.objects.all()
     paginator = Paginator(posts1,5) 
     now_page = request.GET.get('page')
     posts1 = paginator.get_page(now_page)
@@ -55,7 +55,7 @@ def CScreate(request):
         return render(request,'CScreate.html')
 
     elif request.method == "POST":
-        post1 = Dongne1()
+        post1 = Dongne2()
         post1.user=request.user
         post1.title = request.POST['title']
         post1.content = request.POST['content']
@@ -72,7 +72,7 @@ def CScreate(request):
         return render(request,'index.html')
 
 def CSread(request,post_id): 
-    post = Dongne1.objects.get(id = post_id)
+    post = Dongne2.objects.get(id = post_id)
     context={
         "post":post
     }
@@ -80,14 +80,14 @@ def CSread(request,post_id):
 
 def CSupdate(request,post_id):  
     if request.method == "GET":
-        post = Dongne1.objects.get(id = post_id)
+        post = Dongne2.objects.get(id = post_id)
         context = {
             "post":post,
         }
         return render(request,'CSupdate.html',context)
 
     elif request.method == "POST":
-        post = Dongne1.objects.get(id = post_id)
+        post = Dongne2.objects.get(id = post_id)
         post.title = request.POST['title']
         post.content = request.POST['content']
         post.category=request.POST['category']
@@ -96,7 +96,7 @@ def CSupdate(request,post_id):
         return redirect(index)
 
 def CSdelete(request,post_id):
-    post = Dongne1.objects.get(id=post_id)
+    post = Dongne2.objects.get(id=post_id)
     post.delete()
     return redirect(index)
 # Create your views here.
