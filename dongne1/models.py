e69bcafebc528c262baa4cd49b9ebbcecedba0ac
Hhaeum <yellowgreeting@gmail.com>
2019-08-09 15:58:11 +0900

    3

--- dongne1/models.py
+++ dongne1/models.py
@@ -8,6 +8,7 @@ class Dongne1(models.Model):
     created_at = models.DateField(auto_now_add= True)
     updated_at = models.DateField(auto_now = True)
     category = models.CharField(max_length=50,blank=True)
+    anonymous = models.BooleanField(default=False)
     # user = models.ForeignKey(User,on_delete=models.CASCADE)
     pic=models.ImageField(upload_to="image/",null=True)
 
